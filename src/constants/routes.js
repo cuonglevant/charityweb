4b3c482c7ceee7610f4de5ead10a9f647c1b539f
Cuong-Levant <cuonglevant@gmail.com>
2023-11-07 15:35:02 +0700

    feat/volunteer&account(undone)

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,13 @@
 export const ROUTES = {
+<<<<<<< HEAD
   HOME_PATH: "/",
   DONATION_PATH: "/donation",
+=======
+
+  HOME_PATH: "/",
+  DONATION_PATH: "/donation",
+  ACCOUNT_PATH: "/account",
+  VOLUNTEER_PATH: "/volunteer",
+
+>>>>>>> e5ad887 (feat/volunteer&account(undone))
 };
