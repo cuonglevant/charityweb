bebbdcb6bf36fbb760b923d56903957d7dd173d3
BernieCane <hnbangdeptrai@gmail.com>
2023-11-15 22:16:01 +0700

    notyet

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from "react";
 
 import "./index.css";
 import Donation from "./pages/Donation";
-<<<<<<< HEAD
-=======
 import "./pages/Account/Account.css";
 import Home from "./pages/Home";
 import Account from "./pages/Account";
 import Volunteer from "./pages/Volunteer";
->>>>>>> e5ad887 (feat/volunteer&account(undone))
 import { ROUTES } from "./constants/routes";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -18,11 +15,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path={ROUTES.DONATION_PATH} element={<Donation />} />
-<<<<<<< HEAD
-=======
           <Route path={ROUTES.ACCOUNT_PATH} element={<Account />} />
           <Route path={ROUTES.VOLUNTEER_PATH} element={<Volunteer />} />
->>>>>>> e5ad887 (feat/volunteer&account(undone))
         </Routes>
       </BrowserRouter>
     </div>
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,13 +1,8 @@
 export const ROUTES = {
-<<<<<<< HEAD
-  HOME_PATH: "/",
-  DONATION_PATH: "/donation",
-=======
 
   HOME_PATH: "/",
   DONATION_PATH: "/donation",
   ACCOUNT_PATH: "/account",
   VOLUNTEER_PATH: "/volunteer",
 
->>>>>>> e5ad887 (feat/volunteer&account(undone))
 };
