cd1f5a1f9b3153039a97b6d0f8d2a8e882dfd73c
tuanbao_albion <bao2732003@gmail.com>
2023-11-15 08:20:25 +0700

    feat: update homepage

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,8 @@
 export const ROUTES = {
   HOME_PATH: "/",
   DONATION_PATH: "/donation",
+  LOGIN_PATH: "/Login",
+  DONATION_PATH: "/donation",
+  ACCOUNT_PATH: "/account",
+  VOLUNTEER_PATH: "/volunteer",
 };
