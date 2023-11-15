0210455fe4dbb6da91c6885ff9fb584a78130582
BernieCane <106719540+BernieCane@users.noreply.github.com>
2023-11-15 21:59:48 +0700

    Revert "feat/volunteer&account(undone)"

--- src/App.js
+++ src/App.js
@@ -2,13 +2,6 @@ import React from "react";
 
 import "./index.css";
 import Donation from "./pages/Donation";
-<<<<<<< HEAD
-=======
-import "./pages/Account/Account.css";
-import Home from "./pages/Home";
-import Account from "./pages/Account";
-import Volunteer from "./pages/Volunteer";
->>>>>>> e5ad887 (feat/volunteer&account(undone))
 import { ROUTES } from "./constants/routes";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -18,11 +11,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path={ROUTES.DONATION_PATH} element={<Donation />} />
-<<<<<<< HEAD
-=======
-          <Route path={ROUTES.ACCOUNT_PATH} element={<Account />} />
-          <Route path={ROUTES.VOLUNTEER_PATH} element={<Volunteer />} />
->>>>>>> e5ad887 (feat/volunteer&account(undone))
         </Routes>
       </BrowserRouter>
     </div>
